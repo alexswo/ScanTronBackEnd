a4e8c22a345e2d1c262e9fbe4dc64862a28fdcbb
alexswo <alexswo@g.ucla.edu>
2019-05-10 13:28:32 -0700

    woo

--- routes/reviews.js
+++ routes/reviews.js
@@ -9,53 +9,55 @@ var express = require('express');
 var router = express.Router();
 
 /* GET reviews listing. */
-router.get('/', function(req, res, next) {
-    Review.find({}).then(eachOne => {
-      res.json(eachOne);
-    })
+router.get('/', function (req, res, next) {
+  Review.find({}).then(eachOne => {
+    res.json(eachOne);
+  })
 });
 
 /* POST new review. */
-router.post('/', function(req, res) {
-    givenReview = new Review({
-      rating: req.body.Rating,
-      reviewer: req.body.Reviewer,
-      reviewee: req.body.Reviewee
-    });
+router.post('/', function (req, res) {
+  givenReview = new Review({
+    rating: req.body.Rating,
+    reviewer: req.body.Reviewer,
+    reviewee: req.body.Reviewee
+  });
 
-      givenReview.save(function(err, review) {
-        if (err) return res.send(err); //throw err;
+  givenReview.save(function (err, review) {
+    if (err) return res.send(err); //throw err;
 
-        User.getUserByID(review.reviewer).then((reviewer) => {
-          //reviewer.writeReview(review);
+    User.getUserByID(review.reviewer).then((reviewer) => {
+      //reviewer.writeReview(review);
 
-          User.getUserByID(review.reviewee).then((reviewee) => {
-            //reviewee.receiveReview(review);
+      User.getUserByID(review.reviewee).then((reviewee) => {
+        //reviewee.receiveReview(review);
 
-            Producer = kafka.Producer,
-            client = new kafka.KafkaClient({kafkaHost: '127.0.0.1:9092'}),
-            producer = new Producer(client);
+        Producer = kafka.Producer,
+          client = new kafka.KafkaClient({ kafkaHost: '127.0.0.1:9092' }),
+          producer = new Producer(client);
 
-            userId = new kafka.KeyedMessage('userId', req.body.Reviewer)
-            chefId = new kafka.KeyedMessage('chefId', req.body.Reviewee)
-            ratingMessage = new kafka.KeyedMessage('rating', req.body.Rating)
+        userId = new kafka.KeyedMessage('userId', req.body.Reviewer)
+        chefId = new kafka.KeyedMessage('chefId', req.body.Reviewee)
+        ratingMessage = new kafka.KeyedMessage('rating', req.body.Rating)
 
-            producer.send([{ topic: 'rating',
-                    messages: `{"userId": ${req.body.Reviewer}, "chefId": ${req.body.Reviewee}, "rating": ${req.body.Rating}}`,
-                    }], function (err, data) {
-              console.log(data);
+        producer.send([{
+          topic: 'rating',
+          messages: `{"userId": ${req.body.Reviewer}, "chefId": ${req.body.Reviewee}, "rating": ${req.body.Rating}}`,
+        }], function (err, data) {
+          console.log(data);
 
-              // GET to flask server to receive the rating
-              request.get({url: 'http://localhost:5000'
-              }, (err, response, data) => {
-                console.log("Updated recommendation engine");
-              });
-            });
-          })
-        })
+          // GET to flask server to receive the rating
+          request.get({
+            url: 'http://localhost:5000'
+          }, (err, response, data) => {
+            console.log("Updated recommendation engine");
+          });
+        });
+      })
+    })
 
-        res.json(givenReview);
-    });
+    res.json(givenReview);
+  });
 });
 
 module.exports = router;
